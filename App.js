dc54eb92ebe671b5abfe6925f0c3ef7f70cb4205
vishwarajk <49063973+vishwarajk@users.noreply.github.com>
2024-04-25 17:58:36 +0530

    Create App.js

---
+++ App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import './App.css';
+import Form from './Form';
+import Table from './Table';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Code Snippet App</h1>
+      <Form />
+      <Table />
+    </div>
+  );
+}
+
+export default App;
